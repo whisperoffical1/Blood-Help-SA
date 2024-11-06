25d1bfc7f1f981c0532024a7037eca815a28c6e6
BLD-Scripting <153807479+whisperoffical1@users.noreply.github.com>
2024-11-06 19:20:09 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.1'
+name 'Help'
+description 'A free command to respond to whatever you put in the chat.'
+author 'BloodXGorr'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
